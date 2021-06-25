35149223a6d16d323f55915304cfdb5972c52b96
ukrmaxim <ukrmaxim@protonmail.com>
2021-06-25 16:39:23 +0300

    Add GamesController#help test

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -69,7 +69,6 @@ RSpec.describe GamesController, type: :controller do
     it 'creates game' do
       # сперва накидаем вопросов, из чего собирать новую игру
       generate_questions(15)
-
       post :create
       game = assigns(:game) # вытаскиваем из контроллера поле @game
 
@@ -86,9 +85,9 @@ RSpec.describe GamesController, type: :controller do
       params = { id: game_w_questions.id }
       get :show, params: params
       game = assigns(:game) # вытаскиваем из контроллера поле @game
+
       expect(game.finished?).to be false
       expect(game.user).to eq(user)
-
       expect(response.status).to eq(200) # должен быть ответ HTTP 200
       expect(response).to render_template('show') # и отрендерить шаблон show
     end
@@ -144,14 +143,48 @@ RSpec.describe GamesController, type: :controller do
     end
 
     it 'create a game while the previous game is not finished' do
+      game = assigns(:game)
+
       expect(game_w_questions.finished?).to be false
       expect { post :create }.to change(Game, :count).by(0)
-
-      game = assigns(:game)
       expect(game).to be nil
-
       expect(response).to redirect_to(game_path(game_w_questions))
       expect(flash[:alert]).to be
+    end
+
+    it 'uses audience help' do
+      params = { id: game_w_questions.id, help_type: :audience_help }
+
+      expect(game_w_questions.current_game_question.help_hash[:audience_help]).not_to be
+      expect(game_w_questions.audience_help_used).to be_falsey
+
+      put :help, params: params
+      game = assigns(:game)
+
+      # проверяем, что игра не закончилась, что флажок установился, и подсказка записалась
+      expect(game.finished?).to be_falsey
+      expect(game.audience_help_used).to be_truthy
+      expect(game.current_game_question.help_hash[:audience_help]).to be
+      expect(game.current_game_question.help_hash[:audience_help].keys).to contain_exactly('a', 'b', 'c', 'd')
+      expect(response).to redirect_to(game_path(game))
+    end
+
+    it 'can use fifty_fifty' do
+      params = { id: game_w_questions.id, help_type: :fifty_fifty }
+
+      expect(game_w_questions.current_game_question.help_hash[:fifty_fifty]).not_to be
+      expect(game_w_questions.fifty_fifty_used).to be false
+
+      put :help, params: params
+      game = assigns(:game)
+      cgq = game.current_game_question
+
+      expect(game.finished?).to be false
+      expect(game.fifty_fifty_used).to be true
+      expect(cgq.help_hash[:fifty_fifty]).to be
+      expect(cgq.help_hash[:fifty_fifty]).to include(cgq.correct_answer_key)
+      expect(game.current_game_question.help_hash[:fifty_fifty].size).to eq(2)
+      expect(response).to redirect_to(game_path(game))
     end
   end
 end
