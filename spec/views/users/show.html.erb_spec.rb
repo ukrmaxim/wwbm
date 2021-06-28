871f1ddfeb9f4bf5c59e60b63c7ac5aca43d24d5
ukrmaxim <ukrmaxim@protonmail.com>
2021-06-28 15:33:16 +0300

    Add test to view .show

--- spec/views/users/show.html.erb_spec.rb
+++ spec/views/users/show.html.erb_spec.rb
@@ -1,15 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe 'users/show', type: :view do
-  let(:user) { FactoryBot.build_stubbed(:user, name: 'Макс', balance: 5000) }
+  before(:each) do
+    @user = assign(:user, FactoryBot.create(:user, name: 'Saimon'))
 
-  it 'renders user name' do
     render
-    rendered.should contain('Макс')
   end
 
-  it 'renders change password' do
-    user.current_user?
-    expect(rendered).to match 'Сменить имя и пароль'
+  context 'Login user' do
+    before(:each) do
+      sign_in(@user)
+
+      render
+    end
+
+    it 'renders user name' do
+      expect(rendered).to match 'Saimon'
+    end
+
+    it 'renders change password' do
+      expect(rendered).to match 'Сменить имя и пароль'
+    end
+
+    it 'rendered fragments with the game' do
+      assign(:games, [FactoryBot.build_stubbed(:game)])
+      stub_template 'users/_game.html.erb' => 'Game fragments'
+
+      render
+
+      expect(rendered).to have_content 'Game fragments'
+    end
+  end
+
+  context 'Anonymous user' do
+    it "don't renders change password" do
+      expect(rendered).not_to match 'Сменить имя и пароль'
+    end
   end
 end
